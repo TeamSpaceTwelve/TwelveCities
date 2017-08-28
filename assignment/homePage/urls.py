46a266846896001c69cfe3ae80592d5121c21480
Spoopsta <nicka98@live.com.au>
2017-08-28 22:19:07 +1000

    Beginning of website
    Basic homepage has been implemented.

---
+++ assignment/homePage/urls.py
@@ -0,0 +1,8 @@
+# homePage/urls.py
+from django.conf.urls import url
+from homePage import views
+
+urlpatterns = [
+   url(r'^$', views.HomePageView.as_view()),
+
+]
---
+++ assignment/homePage/views.py
@@ -0,0 +1,9 @@
+# Create your views here.
+# homePage/views.py
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'homePage.html', context=None)
