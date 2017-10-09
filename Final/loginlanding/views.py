66168eb0b547ee01780f1c4e7f8fa9f325d48c01
Spoopsta <nicka98@live.com.au>
2017-10-09 14:17:30 +1000

    fused version of the assignment

---
+++ Final/loginlanding/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import facilities
+
+
+def index(request):
+    all_facilities = facilities.objects.all()
+    template = loader.get_template('loginlanding/index.html')
+
+    context = {
+        'all_facilities': all_facilities,
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def hoteldetail(request, specific_id):
+    template = loader.get_template('loginlanding/detail.html')
+    obj_details = Hotels.objects.filter(id=specific_id),
+    context = {
+        'obj_details': obj_details,
+    }
+    return HttpResponse(template.render(context, request))
+
+
