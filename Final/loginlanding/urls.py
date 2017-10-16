e39a8cd0beb685f192d3cc45c305df391e8d907d
DaleRoy01 <30398874+DaleRoy01@users.noreply.github.com>
2017-10-16 14:45:00 +1000

    Add files via upload
    I did things

--- Final/loginlanding/urls.py
+++ Final/loginlanding/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url('(?P<specific_id>[0-9]+)/$', views.hoteldetail, name='hoteldetail'),
-    url('search/(?P<item>[a-z,A-Z,0-9]+)/$', views.results, name='results')
+    url('item/(?P<specific_id>[0-9]+)/$', views.hoteldetail, name='hoteldetail'),
+    url('search/(?P<item>[a-z,A-Z,0-9]+)/$', views.results, name='results'),
+    url('(?P<id>[a-z,A-Z,0-9]+)/$', views.index2, name='index'),
+    url('(?P<id>[a-z,A-Z,0-9]+)/search/(?P<item>[a-z,A-Z,0-9]+)/$', views.results2, name='results'),
 ]
--- Final/loginlanding/views.py
+++ Final/loginlanding/views.py
@@ -28,6 +28,27 @@ def index(request):
     #return HttpResponse(template.render(context, request), {'form':form})
     return render(request, 'loginlanding/index.html', {'all_facilities': all_facilities, 'form': form})
 
+
+def index2(request, id):
+    if request.method == 'POST':
+        form = search(request.POST)
+        if form.is_valid():
+            userSearch = form.data['search']
+            return redirect('/item/' + id + '/search/' + userSearch, permanent=True)
+    else:
+        form = search()
+
+    all_facilities = facilities.objects.all()
+    template = loader.get_template('loginlanding/index.html')
+
+    context = {
+        'all_facilities': all_facilities,
+    }
+
+    # return HttpResponse(template.render(context, request), {'form':form})
+    return render(request, 'loginlanding/index.html', {'all_facilities': all_facilities, 'form': form})
+
+
 def hoteldetail(request, specific_id):
     template = loader.get_template('loginlanding/detail.html')
     obj_details = facilities.objects.filter(id=specific_id),
@@ -49,3 +70,18 @@ def results(request, item):
     else:
         form = search()
     return render(request, 'loginlanding/search.html', {'all_facilities': all_facilities, 'form': form})
+
+def results2(request, item, id):
+    all_facilities = facilities.objects.filter(name__icontains=item)
+    print(all_facilities)
+    print(item)
+    #all_facilities = facilities.objects.all()
+    if request.method == 'POST':
+        form = search(request.POST)
+        if form.is_valid():
+            userSearch = form.data['search']
+            return redirect('/item/' + id + 'search/'+userSearch, permanent=True)
+    else:
+        form = search()
+    return render(request, 'loginlanding/search.html', {'all_facilities': all_facilities, 'form': form})
+
